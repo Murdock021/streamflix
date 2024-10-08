e7f11802b83a2f8bb73ba1dc819e70a201288099
Murdock021 <diogomachadocmb@gmail.com>
2024-10-08 11:51:16 -0300

    Feat: create contructor

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -10,9 +10,17 @@ describe("Category Unit Test", () => {
       is_active: true,
       created_at,
     });
-    expect(category.name).toBe("Movie");
+
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      description: "description",
+      is_active: true,
+      created_at,
+    });
+
+    /*  expect(category.name).toBe("Movie");
     expect(category.description).toBe("description");
     expect(category.is_active).toBeTruthy();
-    expect(category.created_at).toBe(created_at);
+    expect(category.created_at).toBe(created_at); */
   });
 });
